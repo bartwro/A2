14e7e107b449bf69f8b9bfb47f7119e001074cc1
Bartosz Starczewski <bartosz.starczewski@gmail.com>
2019-11-13 23:28:42 +0100

    Scaffolding of scaffolding.

--- a2-project/src/app/app-routing.module.ts
+++ a2-project/src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShellComponent } from './home/shell/shell.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 const routes: Routes = [
   {
     path: '',
-    //component: ShellComponent
+    component: ShellComponent, // this is for some trick with routing..
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      // {
+      //   path: 'items',        
+      // }
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'}
+    ]
   }
-
 ];
 
 @NgModule({
--- a2-project/src/app/app.module.ts
+++ a2-project/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './home/menu/menu.component';
+import { ShellComponent } from './home/shell/shell.component';
+import { PageNotFoundComponent } from './home/page-not-found/page-not-found.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ShellComponent,
+    PageNotFoundComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
